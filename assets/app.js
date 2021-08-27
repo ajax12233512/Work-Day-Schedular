81fb1960127a8332cc3f8838d243e094d040dd12
Neil Juanillo <neilj1213@outlook.com>
2021-08-26 21:24:20 -0500

    Time slots are color coded correctly

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,41 @@
-
 //Display current date and time
-var today = moment().format('MMMM Do YYYY');
-console.log(today);
-$('#currentDay').text(today);
+// var currentHour = moment().format('hh:mm a'); //gets current hour
+// console.log(currentHour);
+
+//Displays current date to header
+var today = moment();
+var displayToday = today.format('dddd, MMMM Do YYYY');
+$('#currentDay').text(displayToday)
+
+var $slots = $('.container').children();
+console.log($slots);
+
+var testTime = moment('10am', 'h:mma');
+
+// console.log($9am.attr('id'));
+$slots.each(function(){
+    var time = moment($(this).attr('id'), 'h:mma');//get the time that represents each slot
+
+    if(time.isBefore(testTime)){
+        $(this).children('input').css('background-color', 'lightgrey');
+    } 
+    else if(time.isSame(testTime))
+    {
+        $(this).children('input').css('background-color', 'red');
+    } 
+    else if(time.isAfter(testTime))
+    {
+        $(this).children('input').css('background-color', 'green');
+    }   
+});
+
+// var onePm = moment('11am', 'h:mma');
+// // var isBefore = 
+// console.log(onePm.toString());
+
+
+
+
+
+// var onePm = moment('03:00pm', 'hh:mma');
+// console.log(onePm);
