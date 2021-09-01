851feb7427a0ccec153abcde21eeafeffe4e20e7
Neil Juanillo <neilj1213@outlook.com>
2021-09-01 00:11:19 -0500

    added more robust save system

--- assets/app.js
+++ assets/app.js
@@ -19,29 +19,69 @@ $slots.each(function(){
 
     if(time.hour() == today.hour())
     {
-        $(this).children('input').css('background-color', 'red');
-        $(this).children('input').css('color', 'white');
+        $(this).children('input').addClass('present');
+        // $(this).children('input').css('color', 'white');
     } 
     else if(time.isBefore(today)){
-        $(this).children('input').css('background-color', 'lightgrey');
+        $(this).children('input').addClass('past');
     } 
     else if(time.isAfter(today))
     {
-        $(this).children('input').css('background-color', 'green');
+        $(this).children('input').addClass('future');
         $(this).children('input').css('color', 'white');
     }
 });
 
 var myStorage = window.localStorage;
 var $inputSlots = $slots.children('input');
-
+console.log($inputSlots);
 var saveButtons = $('button');
 
 saveButtons.on('click', function(){
     var input = $(this).parent().children('input').val();
     localStorage.setItem($(this).parent().children('h2').text(), input);
-})
+    console.log(myStorage);
+});
+
+
+
+$inputSlots.each(function(){
+    var thisName = $(this).parent().children('h2').text()
+    if(myStorage.getItem(thisName) == null)
+        $(this).val(' ');
+    else
+    {
+        var savedItem = myStorage.getItem(thisName)
+        if($(this).val() != myStorage.getItem(thisName))
+        {
+            console.log("made it here");
+            $(this).val(savedItem);
+        }
+    }
+    
+});
+
 
 
 //Add functionality to have the input clear itself if it is NOT saved
+// $(function(){
+
+//     myStorage.getItem()
+
+//     var x = $('#11am').children('input').val();
+//     console.log(x);
+// })
+
+// var x = $slots.children('input');
+// var xLength = x.length;
+
+// console.log(x)
+// console.log("x lenght:" + xLength);
+
+// var localStorageArray = [];
+
+// x.each(function(){
+//     var timeName = $(this).parent().children('h2').text();
+    
+// })
 
